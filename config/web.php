b70cb5fd4678deae9f6ed32577acb27e442b89e0
zjm <zjmwy2016@163.com>
2018-04-02 14:51:49 +0800

    cookie error fix

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'zjmtest',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
